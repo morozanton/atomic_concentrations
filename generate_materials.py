dcb3da363f28d4f063c2638cdaf0b2f8f97072ec
Anton Moroz <morozanton@users.noreply.github.com>
2025-04-24 12:07:04 +0200

    Fixed for compound materials

--- generate_materials.py
+++ generate_materials.py
@@ -14,52 +14,50 @@ class ConcentrationConvertor:
 
         self.formula = input("Chemical composition (e.g. 'H2O', 'C2H4 B0.2'):\n---> ").strip()
         self.density = float(input("Density (g/cc): ").strip())
+        name = input("Material name (leave blank to use chemical formula): ").strip()
+
+        self.material_name = name if name else self.formula
         print()
 
         self.calculate(self.formula, self.density)
 
     @staticmethod
     def parse_formula(formula: str):
-        matches = re.findall(r"([A-Z][a-z]?)(\d*\.?\d*)", formula)
-        return {m[0]: float(m[1]) if m[1] else 1 for m in matches}
+        components = formula.split()
+        composition = []
+
+        for c in components:
+            matches = re.findall(r"([A-Z][a-z]?)(\d*\.?\d*)", c)
+            composition.append({m[0]: float(m[1]) if m[1] else 1 for m in matches})
+
+        return composition
 
     @staticmethod
     def get_isotope_code(atomic_number, mass_number) -> str:
         mass_formatted = f"{int(mass_number):03}"
         return str(int(atomic_number)) + mass_formatted
 
-    def calculate(self, formula, density) -> None:
-        elements = self.parse_formula(formula)
+    def calculate(self, formula: str, density: float) -> None:
+        output = [f"m1\t\t$ {self.material_name}; {density} g/cc\n"]
 
-        compound_mass = 0
-        purity_factor = 1
-        for el, num in elements.items():
-            if num < 1:
-                purity_factor -= num
-                continue
-            mass = self.data[self.data["element"] == el]["element_atomic_weight"].dropna().iloc[0]
-            compound_mass += mass * num
-        compound_mass /= purity_factor
+        components = self.parse_formula(formula)
+        for c in components:
+            component_mass = sum(self.data[self.data["element"] == el]["element_atomic_weight"].dropna().iloc[0] * (
+                num if num > 1 else 1)
+                                 for el, num in c.items())
 
-        output = [f"m1\t\t$ {formula}; {density} g/cc\n"]
+            common_factor = self.AVOGADRO * density / component_mass
 
-        factor = self.AVOGADRO * density
-        for el, formula_units in elements.items():
+            for el, formula_units in c.items():
+                isotopes = self.data[self.data["element"] == el]
 
-            isotopes = self.data[self.data["element"] == el]
-            if formula_units < 1:
-                formula_units = 1
+                for index, row in isotopes.iterrows():
+                    if pd.notna(row["isotopic_composition"]):
+                        atomic_concentration = f"{(common_factor * row["isotopic_composition"] * formula_units):.6E}"
+                        isotope_code = self.get_isotope_code(atomic_number=row["atomic_number"],
+                                                             mass_number=row["mass_number"])
+                        output.append(f"{isotope_code}\t{atomic_concentration}\n")
 
-            for index, row in isotopes.iterrows():
-                if pd.notna(row["isotopic_composition"]):
-                    atomic_concentration = (factor * row["isotopic_composition"] *
-                                            formula_units / compound_mass)
-
-                    atomic_concentration = f"{atomic_concentration:.6E}"
-
-                    isotope_code = self.get_isotope_code(atomic_number=row["atomic_number"],
-                                                         mass_number=row["mass_number"])
-                    output.append(f"{isotope_code}\t{atomic_concentration}\n")
         output.sort(key=lambda x: int(x[1:].split()[0]))
         print("".join(output))
 
